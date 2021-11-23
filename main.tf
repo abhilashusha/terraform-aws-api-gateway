4c6d01b0fcc31a9f970d26f63bb21e276a5f126d
abhilashusha <abhilash.u.nair@gmail.com>
2021-11-22 21:08:46 -0500

    Corrected the rauthorization scope

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "aws_api_gateway_method" "default" {
   http_method          = element(var.http_methods, count.index)
   authorization        = length(var.authorizations) > 0 ? element(var.authorizations, count.index) : "NONE"
   authorizer_id        = length(var.authorizer_ids) > 0 ? element(var.authorizer_ids, count.index) : (var.authorizer_count > 0 ? aws_api_gateway_authorizer.default.*.id[count.index] : null)
-  authorization_scopes = length(var.authorization_scopes) > 0 ? element(var.authorization_scopes, count.index) : null
+  authorization_scopes = length(var.authorization_scopes) > 0 ? [element(var.authorization_scopes, count.index)]: null
   api_key_required     = length(var.api_key_requireds) > 0 ? element(var.api_key_requireds, count.index) : null
   request_models       = length(var.request_models) > 0 ? element(var.request_models, count.index) : { "application/json" = "Empty" }
   request_validator_id = length(var.request_validator_ids) > 0 ? element(var.request_validator_ids, count.index) : null
